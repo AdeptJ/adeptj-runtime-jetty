8ed5a19fb3136f924aa4e7ecde8cdc553ab3c662
Rakesh Kumar <kumar_rakesh@live.in>
2022-08-29 09:54:56 +0530

    Added ServletInfo for Servlet registration.

--- src/main/java/com/adeptj/runtime/jetty/JettyServer.java
+++ src/main/java/com/adeptj/runtime/jetty/JettyServer.java
@@ -3,15 +3,14 @@ package com.adeptj.runtime.jetty;
 import com.adeptj.runtime.kernel.AbstractServer;
 import com.adeptj.runtime.kernel.SciInfo;
 import com.adeptj.runtime.kernel.ServerName;
+import com.adeptj.runtime.kernel.ServletDeployment;
+import com.adeptj.runtime.kernel.ServletInfo;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContainerInitializerHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
-
-import javax.servlet.http.HttpServlet;
-import java.util.List;
 
 public class JettyServer extends AbstractServer {
 
@@ -25,7 +24,7 @@ public class JettyServer extends AbstractServer {
     }
 
     @Override
-    public void start(String[] args, SciInfo sciInfo) {
+    public void start(String[] args, ServletDeployment deployment) {
         int maxThreads = 100;
         int minThreads = 10;
         int idleTimeout = 120;
@@ -36,17 +35,16 @@ public class JettyServer extends AbstractServer {
         this.jetty.addConnector(connector);
         this.context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         this.context.setContextPath("/");
+        SciInfo sciInfo = deployment.getSciInfo();
         this.context.addServletContainerInitializer(new ServletContainerInitializerHolder(sciInfo.getSciInstance(),
                 sciInfo.getHandleTypesArray()));
-        ServletHolder greetingServlet = new ServletHolder("GreetingServlet", GreetingServlet.class);
-        this.context.addServlet(greetingServlet, "/greet");
+        this.registerServlets(deployment.getServletInfos());
         this.jetty.setHandler(this.context);
         try {
             this.jetty.start();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        this.doStart(args, "AdeptJ Jetty Terminator");
     }
 
     @Override
@@ -59,12 +57,7 @@ public class JettyServer extends AbstractServer {
     }
 
     @Override
-    public void registerServlets(List<Class<? extends HttpServlet>> servlets) {
-        int count = 0;
-        for (Class<? extends HttpServlet> servlet : servlets) {
-            ServletHolder greetingServlet = new ServletHolder(servlet.getSimpleName(), servlet);
-            this.context.addServlet(greetingServlet, "/servlet" + count);
-            count++;
-        }
+    protected void doRegisterServlet(ServletInfo info) {
+        this.context.addServlet(new ServletHolder(info.getServletName(), info.getServletClass()), info.getPath());
     }
 }
